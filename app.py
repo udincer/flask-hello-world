2ba235c5742f737da6db0949868991712825629d
tev <umutdincer@gmail.com>
2022-02-24 20:52:44 -0800

    test

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!!! Hi.'
+
+@app.route('/hello')
+def hello_world():
+    return 'Crazy'
+
+
